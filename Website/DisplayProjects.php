6d797cfa51560fe27a6a1a645ef08998fbe1dae3
ImageFaded <TBWalton@dundee.ac.uk>
2018-03-20 11:44:09 +0000

    Displays Projects

---
+++ Website/DisplayProjects.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="FormStyle_StaffPage.css">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+      <body>         
+       <h1>All Projects</h1>
+       <div class="Options">
+           <a href="#"> New Project</br></a>
+           <a href="https://zeno.computing.dundee.ac.uk/2017-agile/team1/DisplayProjects.php"> All Projects</br></a>
+           <a href="#"> Pending Projects</br></a>
+           <a href="#"> Completed Projects</br></a>
+           <a href='https://zeno.computing.dundee.ac.uk/2017-agile/team1/'> Log Out </br></a>
+       </div>
+        <?php
+        // put your code here
+       /* session_start();
+       echo $_COOKIE["id"];
+        echo $_COOKIE["state"];*/
+        ?>
+    </body>
+</html>
